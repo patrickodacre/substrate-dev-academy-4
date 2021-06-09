72ac4b4126a2356d1ecec9de7c6843711177f9fc
pwho <patrick@patrickwho.me>
2021-06-09 15:34:57 -0400

    Make Randomness configurable

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -35,6 +35,7 @@ impl Kitty {
 
 pub trait Config: frame_system::Config {
     type Event: From<Event<Self>> + Into<<Self as frame_system::Config>::Event>;
+    type Randomness: Randomness<Self::Hash>;
 }
 
 decl_storage! {
@@ -133,7 +134,7 @@ impl<T: Config> Module<T> {
 
     fn random_value(sender: &T::AccountId) -> [u8; 16] {
         let payload = (
-            <pallet_randomness_collective_flip::Module<T> as Randomness<T::Hash>>::random_seed(),
+            T::Randomness::random_seed(),
             &sender,
             <frame_system::Module<T>>::extrinsic_index(),
         );
--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -1,65 +1,83 @@
 use super::*;
 
 use crate as kitties;
+use frame_support::{assert_noop, assert_ok, parameter_types, traits::Randomness};
 use sp_core::H256;
-use frame_support::{parameter_types, assert_ok, assert_noop};
 use sp_runtime::{
-	traits::{BlakeTwo256, IdentityLookup}, testing::Header,
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
 };
+use std::cell::RefCell;
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system::{Module, Call, Config, Storage, Event<T>},
-		KittiesModule: kitties::{Module, Call, Storage, Event<T>},
-	}
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system::{Module, Call, Config, Storage, Event<T>},
+        KittiesModule: kitties::{Module, Call, Storage, Event<T>},
+    }
 );
 
 parameter_types! {
-	pub const BlockHashCount: u64 = 250;
-	pub const SS58Prefix: u8 = 42;
+    pub const BlockHashCount: u64 = 250;
+    pub const SS58Prefix: u8 = 42;
 }
 
 impl frame_system::Config for Test {
-	type BaseCallFilter = ();
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type Origin = Origin;
-	type Call = Call;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type Event = Event;
-	type BlockHashCount = BlockHashCount;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = ();
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = SS58Prefix;
+    type BaseCallFilter = ();
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type Origin = Origin;
+    type Call = Call;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type Event = Event;
+    type BlockHashCount = BlockHashCount;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = ();
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = SS58Prefix;
+}
+
+pub struct MockRandom;
+
+thread_local! {
+    static RANDOM_PAYLOAD: RefCell<H256> = RefCell::new(Default::default());
+}
+
+impl Randomness<H256> for MockRandom {
+    fn random(_subject: &[u8]) -> H256 {
+        RANDOM_PAYLOAD.with(|v| *v.borrow())
+    }
 }
 
 impl Config for Test {
-	type Event = Event;
+    type Event = Event;
+    type Randomness = MockRandom;
 }
 
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    let mut t: sp_io::TestExternalities = frame_system::GenesisConfig::default().build_storage::<Test>().unwrap().into();
-    t.execute_with(|| System::set_block_number(1) );
+    let mut t: sp_io::TestExternalities = frame_system::GenesisConfig::default()
+        .build_storage::<Test>()
+        .unwrap()
+        .into();
+    t.execute_with(|| System::set_block_number(1));
     t
 }
 
@@ -72,19 +90,27 @@ fn can_create() {
     new_test_ext().execute_with(|| {
         assert_ok!(KittiesModule::create(Origin::signed(100)));
 
-        let kitty = Kitty([59, 250, 138, 82, 209, 39, 141, 109, 163, 238, 183, 145, 235, 168, 18, 122]);
+        let kitty = Kitty([
+            59, 250, 138, 82, 209, 39, 141, 109, 163, 238, 183, 145, 235, 168, 18, 122,
+        ]);
 
         assert_eq!(KittiesModule::kitties(100, 0), Some(kitty.clone()));
         assert_eq!(KittiesModule::next_kitty_id(), 1);
 
-        assert_eq!(last_event(), Event::kitties(crate::Event::<Test>::KittyCreated(100, 0, kitty)));
+        assert_eq!(
+            last_event(),
+            Event::kitties(crate::Event::<Test>::KittyCreated(100, 0, kitty))
+        );
     });
 }
 
 #[test]
 fn gender() {
     assert_eq!(Kitty([0; 16]).gender(), KittyGender::Male);
-    assert_eq!(Kitty([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).gender(), KittyGender::Female);
+    assert_eq!(
+        Kitty([1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).gender(),
+        KittyGender::Female
+    );
 }
 
 #[test]
@@ -96,23 +122,37 @@ fn can_breed() {
 
         assert_ok!(KittiesModule::create(Origin::signed(100)));
 
-        assert_noop!(KittiesModule::breed(Origin::signed(100), 0, 11), Error::<Test>::InvalidKittyId);
-        assert_noop!(KittiesModule::breed(Origin::signed(100), 0, 0), Error::<Test>::SameGender);
-        assert_noop!(KittiesModule::breed(Origin::signed(101), 0, 1), Error::<Test>::InvalidKittyId);
+        assert_noop!(
+            KittiesModule::breed(Origin::signed(100), 0, 11),
+            Error::<Test>::InvalidKittyId
+        );
+        assert_noop!(
+            KittiesModule::breed(Origin::signed(100), 0, 0),
+            Error::<Test>::SameGender
+        );
+        assert_noop!(
+            KittiesModule::breed(Origin::signed(101), 0, 1),
+            Error::<Test>::InvalidKittyId
+        );
 
         assert_ok!(KittiesModule::breed(Origin::signed(100), 0, 1));
 
-        let kitty = Kitty([59, 254, 219, 122, 245, 239, 191, 125, 255, 239, 247, 247, 251, 239, 247, 254]);
+        let kitty = Kitty([
+            59, 254, 219, 122, 245, 239, 191, 125, 255, 239, 247, 247, 251, 239, 247, 254,
+        ]);
 
         assert_eq!(KittiesModule::kitties(100, 2), Some(kitty.clone()));
         assert_eq!(KittiesModule::next_kitty_id(), 3);
 
-        assert_eq!(last_event(), Event::kitties(crate::Event::<Test>::KittyBred(100u64, 2u32, kitty)));
+        assert_eq!(
+            last_event(),
+            Event::kitties(crate::Event::<Test>::KittyBred(100u64, 2u32, kitty))
+        );
     });
 }
 
 #[test]
 fn combine_dna_works() {
-	assert_eq!(combine_dna(0b11111111, 0b00000000, 0b00001111), 0b11110000);
-	assert_eq!(combine_dna(0b10101010, 0b11110000, 0b11001100), 0b11100010);
+    assert_eq!(combine_dna(0b11111111, 0b00000000, 0b00001111), 0b11110000);
+    assert_eq!(combine_dna(0b10101010, 0b11110000, 0b11001100), 0b11100010);
 }
